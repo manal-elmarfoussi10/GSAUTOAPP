0ab6189ecc2974b4ace457e871500b677d9aeaaa
manal-elmarfoussi10 <manalelmarfoussi@gmail.com>
2025-09-01 20:13:29 +0100

    save current state

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,15 +7,16 @@ use App\Http\Middleware\CompanyAccess;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'company' => CompanyAccess::class,
+            'company' => \App\Http\Middleware\CompanyAccess::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })// <-- add this
+    ->create();
